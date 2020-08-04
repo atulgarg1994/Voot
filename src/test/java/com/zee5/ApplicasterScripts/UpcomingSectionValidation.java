9b84561506f6355743eb49da05c3a21cd3a8a793
Hitesh <hiteshc94@gmail.com>
2020-08-04 11:12:56 +0530

    Commit 8/4/2020

--- src/test/java/com/zee5/ApplicasterScripts/UpcomingSectionValidation.java
+++ src/test/java/com/zee5/ApplicasterScripts/UpcomingSectionValidation.java
@@ -1,9 +1,11 @@
 package com.zee5.ApplicasterScripts;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import com.business.zee.Zee5ApplicasterBusinessLogic;
+import com.utility.Utilities;
 
 public class UpcomingSectionValidation {
 
@@ -12,22 +14,16 @@ public class UpcomingSectionValidation {
 	@BeforeTest
 	public void AppLaunch() throws InterruptedException {
 		System.out.println("Launching Android App");
-		//Utilities.relaunch = true;	// Clear App Data on First Launch
+		Utilities.relaunch = true;	// Clear App Data on First Launch
 		ZEE5ApplicasterBusinessLogic = new Zee5ApplicasterBusinessLogic("zee");
 	}
-
-//	@Test(priority = 0)
-//	@Parameters({ "userType" }) 	// Hitesh
-//	public void accessDeviceLocation(String userType) throws Exception {
-//		ZEE5ApplicasterBusinessLogic.accessDeviceLocationPopUp("Allow", userType);
-//	}
-
 	
 	@Test(priority = 1)
 	@Parameters({ "userType" })		// Manasa
 	public void Login(String userType) throws Exception {
-//		System.out.println("\nVerify Display Language Screen and login flow for various usertypes");
-//		ZEE5ApplicasterBusinessLogic.navigateToLoginScreen_DisplaylangScreen();
+		System.out.println("\nVerify Display Language Screen and login flow for various usertypes");
+		ZEE5ApplicasterBusinessLogic.accessDeviceLocationPopUp("Allow", userType);
+		ZEE5ApplicasterBusinessLogic.navigateToIntroScreen_DisplaylangScreen();;
 		ZEE5ApplicasterBusinessLogic.ZeeApplicasterLogin(userType);
 	}
 	
@@ -36,5 +32,11 @@ public class UpcomingSectionValidation {
 		System.out.println("\nVerify Upcoming Screen");
 		ZEE5ApplicasterBusinessLogic.upcomingSectionValidation();
 		ZEE5ApplicasterBusinessLogic.upcomingContentValidationWithAPIData();
+	}
+	
+	@AfterTest
+	public void tearDownApp() {
+		System.out.println("\nQuit the App\n");
+		ZEE5ApplicasterBusinessLogic.tearDown();
 	}
 }
