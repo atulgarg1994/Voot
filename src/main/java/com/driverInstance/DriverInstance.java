fc0a32ce9dd789b70f07f9aab0cb688ffeec8ba2
Hitesh C <hitesh.c@igs-india.net>
2021-01-27 18:20:52 +0530

    Commit 27/01/2021

--- src/main/java/com/driverInstance/DriverInstance.java
+++ src/main/java/com/driverInstance/DriverInstance.java
@@ -35,7 +35,7 @@ public class DriverInstance extends Drivertools {
 				util.waitForElementDisplayed(AMDOnboardingScreen.objWaitForSplashScreenDisapear, 240);
 				Instant endTime  = Instant.now();
 				Duration timeElapsed = Duration.between(startTime , endTime  );
-				logger.info("Time taken to the App (millisec): " + timeElapsed.toMillis());
+				logger.info("Time taken to launch the App (millisec)" + timeElapsed.toMillis());
 //				extent.extentLogger("Timer","to the App (millisec): " + timeElapsed.toMillis());
 				break;
 
